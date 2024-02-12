091b9185f3dbe1b2e5d746f149d1519b6a74408b
André Ezequiel <andrelucasmartins@hotmail.com>
2024-02-12 17:48:40 -0300

    bugfix: show component

--- src/components/link-button.tsx
+++ src/components/link-button.tsx
@@ -3,5 +3,5 @@ type LinkButtonProps = LinkProps<string> & {
   title: string
 }
 export function LinkButton ({title, ...rest}: LinkButtonProps) {
-  <Link className="text-slate-300 text-center text-base font-body" {...rest}>{title}</Link>
+  return <Link className="text-slate-300 text-center text-base font-body" {...rest}>{title}</Link>
 }
