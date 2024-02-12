cf772e97490696e1ba0d5cad56f0b488d94418ab
André Ezequiel <andrelucasmartins@hotmail.com>
2024-02-12 02:41:38 -0300

    bugfix: enabled button is selected

--- src/components/category-button.tsx
+++ src/components/category-button.tsx
@@ -12,7 +12,7 @@ export function CategoryButton({ title, isSelected, ...rest} : CategoryProps) {
       className={
         clsx(
           "bg-slate-800 px-4 justify-center rounded-md h-10",
-          isSelected && "border2 border-lime-300"
+          isSelected && "border-2 border-lime-300"
         )
       }
       {...rest}
