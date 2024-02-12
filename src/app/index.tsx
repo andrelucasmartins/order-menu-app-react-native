24b28f6b4425f20e5ddda68e99e5d57e71069840
André Ezequiel <andrelucasmartins@hotmail.com>
2024-02-12 19:02:14 -0300

    feat: create new function to details product and params

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,12 +2,18 @@ import { CategoryButton } from '@/components/category-button';
 import { Header } from '@/components/header';
 import { Product } from '@/components/product';
 import { CATEGORIES, MENU } from '@/utils/data/products';
+import { Link } from 'expo-router';
 import { useRef, useState } from 'react';
 import { FlatList, SectionList, Text, View } from 'react-native';
+import { useCartStore } from '../stores/cart-store';
+
 export default function Home (){
+  const cartStore  = useCartStore()
   const [category, setCategory] = useState(CATEGORIES[0])
 
   const sectionListRef = useRef<SectionList>(null)
+
+  const cartQuantityItems = cartStore.products.reduce((total, product) => total + product.quantity, 0)
 
   function handleCategorySelect(selectedCategory: string) {
     setCategory(selectedCategory);
@@ -23,7 +29,7 @@ export default function Home (){
 
   return (
     <View className="flex-1 pt-8">
-      <Header title="Faça seu pedido" cartQuantityItems={3} />
+      <Header title="Faça seu pedido" cartQuantityItems={cartQuantityItems} />
 
       <FlatList
         data={CATEGORIES}
@@ -46,7 +52,11 @@ export default function Home (){
         sections={MENU}
         keyExtractor={(item) => item.id}
         stickySectionHeadersEnabled={false}
-        renderItem={({ item }) => <Product data={item} />}
+        renderItem={({ item }) => (
+          <Link href={`/product/${item.id}`} asChild>
+            <Product data={item} />
+          </Link>
+        )}
         renderSectionHeader={({ section: { title } }) => (
           <Text className="text-xl text-white font-heading mt-8 mb-3">
             {title}
