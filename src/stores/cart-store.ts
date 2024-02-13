fe20463d58285ce70f72362739de393085f8ee1b
André Ezequiel <andrelucasmartins@hotmail.com>
2024-02-13 14:01:50 -0300

    feat: create remove function, clear function and  persist products

--- src/stores/cart-store.ts
+++ src/stores/cart-store.ts
@@ -1,6 +1,9 @@
 import { ProductProps } from "@/utils/data/products";
-import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
 
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+import { create } from "zustand";
 import * as cartInMemory from './helpers/cart-in-memory';
 
 export type ProductCartProps = ProductProps & {
@@ -10,12 +13,25 @@ export type ProductCartProps = ProductProps & {
 type StateProps = {
   products: ProductCartProps[]
   add: (product: ProductProps) => void
+  remove: (productId: string) => void
+  clear: () => void
 }
 
-export const useCartStore = create<StateProps>((set) => ({
+export const useCartStore = create(
+  persist<StateProps>((set) => ({
   products: [],
   add: (product: ProductProps) => 
     set((state) => ({
     products: cartInMemory.add(state.products, product)
   })),
+
+  remove: (productId: string) => 
+    set((state) => ({
+      products: cartInMemory.remove(state.products, productId),
+    })),
+
+    clear: () => set(() => ({ products: []})),
+}), {
+  name: "order-menu:cart",
+  storage: createJSONStorage(() => AsyncStorage)
 }))
